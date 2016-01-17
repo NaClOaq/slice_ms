2dd7be6b86a42b61b4e81156ea1ef332812c3ab5
NaClOaq <kaede1648@gmail.com>
2016-01-17 15:22:32 +0900

    Replace Copy

--- background.js
+++ background.js
@@ -1,38 +1,38 @@
 (function(){
+function getClickHandler() {
+  return function(info, tab) {
+    if(localStorage["regexp"] == 'true'){
+      var find = new RegExp( localStorage["find"], "g" );
+      var url = info.linkUrl.replace(find,localStorage["replace"]);
+      console.log(info.linkUrl);
+    }else{
+      var url = info.linkUrl.split(localStorage["find"]).join(localStorage["replace"]);
+    };
+    console.log(url);
+    saveToClipboard(url);
+  };
+};
 
 
-// ローカルストレージが設定されていない場合は初期化する。
-if (localStorage['icon'] === undefined) {
-  localStorage['icon'] = 'on.png';
+function saveToClipboard(str) {
+  var textArea = document.createElement("textarea");
+  textArea.style.cssText = "position:absolute;left:-100%";
+
+  document.body.appendChild(textArea);
+
+  textArea.value = str;
+  textArea.select();
+  document.execCommand("copy");
+
+  document.body.removeChild(textArea);
 }
-// 起動時にローカルストレージの値を見てアイコンを設定する。
-chrome.browserAction.setIcon({path:localStorage['icon']});
-// アイコンがクリックされた場合、ローカルストレージを変更して、アイコンも変更する。
-chrome.browserAction.onClicked.addListener(function(tab) {
-  if (localStorage['icon'] === 'off.png') {
-    localStorage['icon'] = 'on.png'
-  } else {
-    localStorage['icon'] = 'off.png'
-  }
-  chrome.browserAction.setIcon({path:localStorage['icon']});
-});
 
-chrome.extension.onRequest.addListener(
-   function(request, sender, sendResponse) {
-      switch(request.action) {
-         case 'getIco':
-         console.log(getIcoFromStorage());
-            sendResponse(getIcoFromStorage());
-            break;
-      }
-   }
-);
-function getIcoFromStorage() {
-    return getItem('icon');
-}
-function getItem(key) { return getStorage().getItem(key); }
-function setItem(key, map) { return getStorage().setItem(key, map); }
-function removeItem(key) { return getStorage().removeItem(key); }
-function getStorage() { return window.localStorage; }
 
-})()
+var parentId = chrome.contextMenus.create({
+  "title" : "Replace Copy",
+  "type" : "normal",
+  "contexts" : ["link"],
+  "onclick" : getClickHandler()
+})
+
+})();
